88bd6799fbe695cb46806324d4b4941446bd5642
Alessandro Rebelo <96748455+whittejr@users.noreply.github.com>
2022-02-20 20:36:40 -0300

    atts

---
+++ C & C++/Data-Structure & Algorithms/1.1 array in ds.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int a[50], size, i;
+
+    printf("Digite os tamanho da array: ");
+    scanf("%d", &size);
+    
+    if (size > 50)
+    {
+        printf("Tamanho de array invalido.");
+        return 1;
+    }
+    else
+    {
+    printf("Digite os elementos da array: "); 
+    }
+
+    for (i = 0; i < size; i++)
+    {
+        scanf("%d", &a[i]);
+    }
+
+    printf("Os elementos da array sao: ");
+
+    for(i = 0; i < size; i++)
+    {
+        printf("%d", a[i]); 
+    }
+    
+}
